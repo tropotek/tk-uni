5d46c4cd995e63c34a2ffbca44b8cc69b5fd6c48
mifsudm <mifsudm@unimelb.edu.au>
2018-03-19 07:23:43 +1100

    ~Auto: Minor Code Updates - 252s-dev

--- Uni/Uri.php
+++ Uni/Uri.php
@@ -79,7 +79,10 @@ class Uri extends \Tk\Uri
 
         $url = self::create('/inst/' . $institution->getHash() . '/' . trim($spec,'/'));
         if ($institution->getDomain()) {
+            $url = self::create($spec);
             $url = $url->setHost($institution->getDomain());
+        } else {
+            $url = self::create('/inst/' . $institution->getHash() . '/' . trim($spec,'/'));
         }
 
         return $url;
