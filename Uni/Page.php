062cb93c3027be11129e219e29521e5bc38cea8a
godar <info@tropotek.com>
2018-07-21 06:43:05 +1000

    ~Auto: Commit

--- Uni/Page.php
+++ Uni/Page.php
@@ -10,36 +10,6 @@ class Page extends \Bs\Page
 {
 
     /**
-     * Set the page heading, should be set from main controller
-     *
-     * @return \Dom\Template
-     * @throws \Tk\Exception
-     * @throws \Exception
-     */
-    public function show()
-    {
-        $template = parent::show();
-
-        if ($this->getUser()) {
-            $template->setAttr('dashUrl', 'href', \Uni\Uri::createHomeUrl('/index.html'));
-        }
-
-        if ($this->getUser()) {
-            $institutionId = $this->getConfig()->getInstitutionId();
-            $subjectId = $this->getConfig()->getSubjectId();
-            $role = $this->getUser()->getRole();
-            $js = <<<JS
-config.subjectId = $subjectId;
-config.institutionId = $institutionId;
-config.role = '$role';
-JS;
-            $template->appendJs($js, array('data-jsl-priority' => -1000));
-        }
-
-        return $template;
-    }
-
-    /**
      * Get the currently logged in user
      *
      * @return \Uni\Db\User
