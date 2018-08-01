b33736c55599e53e73cc84cf5e8159f58d3e3f4d
mifsudm <mifsudm@unimelb.edu.au>
2018-08-02 07:10:59 +1000

    ~Auto: Commit

--- sql/mysql/000001.sql
+++ sql/mysql/000001.sql
@@ -94,3 +94,39 @@ CREATE TABLE IF NOT EXISTS `user_permission` (
 
 
 
+
+
+-- ----------------------------
+--  TEST DATA
+-- ----------------------------
+# INSERT INTO `user` (`role_id`, `institution_id`, `username`, `password` ,`name`, `email`, `active`, `hash`, `modified`, `created`)
+# VALUES
+#   (1, 0, 'admin', MD5(CONCAT('password', MD5('10admin'))), 'Administrator', 'admin@example.com', 1, MD5('10admin'), NOW(), NOW()),
+#   (2, 0, 'unimelb', MD5(CONCAT('password', MD5('20unimelb'))), 'Unimelb Client', 'fvas@unimelb.edu.au', 1, MD5('20unimelb'), NOW(), NOW()),
+#   (3, 1, 'staff', MD5(CONCAT('password', MD5('31staff'))), 'Unimelb Staff', 'staff@unimelb.edu.au', 1, MD5('31staff'), NOW(), NOW()),
+#   (4, 1, 'student', MD5(CONCAT('password', MD5('41student'))), 'Unimelb Student', 'student@unimelb.edu.au', 1, MD5('41student'), NOW(), NOW())
+# ;
+#
+# INSERT INTO `institution` (`user_id`, `name`, `email`, `description`, `logo`, `active`, `hash`, `modified`, `created`)
+#   VALUES
+#     (2, 'The University Of Melbourne', 'admin@unimelb.edu.au', 'This is a test institution for this app', '', 1, MD5('1'), NOW(), NOW())
+# ;
+#
+# INSERT INTO `subject` (`institution_id`, `name`, `code`, `email`, `description`, `date_start`, `date_end`, `modified`, `created`)
+#   VALUES (1, 'Poultry Industry Field Work', 'VETS50001_2014_SM1', 'subject@unimelb.edu.au', '',  NOW(), DATE_ADD(NOW(), INTERVAL 190 DAY), NOW(), NOW() )
+# --  VALUES (1, 'Poultry Industry Field Work', 'VETS50001_2014_SM1', 'subject@unimelb.edu.au', '',  NOW(), DATE_ADD(CURRENT_DATETIME, INTERVAL 190 DAY), NOW(), NOW() )
+# ;
+#
+# INSERT INTO `subject_has_user` (`user_id`, `subject_id`)
+# VALUES
+#   (3, 1),
+#   (4, 1)
+# ;
+#
+# INSERT INTO `subject_pre_enrollment` (`subject_id`, `email`)
+# VALUES
+#   (1, 'student@unimelb.edu.au')
+# ;
+
+
+
