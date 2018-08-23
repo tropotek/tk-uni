9a31aeeb080fd23f5960944eeef225bc2b59e65d
mifsudm <mifsudm@unimelb.edu.au>
2018-08-23 11:42:55 +1000

    ~Auto: Commit

--- sql/000000.php
+++ sql/000000.php
@@ -4,8 +4,10 @@
 $config = \Uni\Config::getInstance();
 $data = \Tk\Db\Data::create();
 
-if(!$data->has('site.title'))
+if(!$data->has('site.title')) {
     $data->set('site.title', 'Uni Base Project');
+    $data->set('site.short.title', 'Tk2Uni');
+}
 if(!$data->has('site.email'))
     $data->set('site.email', 'fvas-elearning@unimelb.edu.au');
 
