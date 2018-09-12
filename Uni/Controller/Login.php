f70181a880882ef2e4a5273f1f609df4c741f545
mifsudm <mifsudm@unimelb.edu.au>
2018-09-12 12:11:19 +1000

    ~Auto: Commit

--- Uni/Controller/Login.php
+++ Uni/Controller/Login.php
@@ -109,7 +109,8 @@ class Login extends Iface
 
         $this->form->appendField(new Field\Input('username'));
         $this->form->appendField(new Field\Password('password'));
-        $this->form->appendField(new Event\Submit('login', array($this, 'doLogin')))->removeCss('btn-default')->addCss('btn btn-lg btn-primary btn-ss');
+        $this->form->appendField(new Event\Submit('login', array($this, 'doLogin')))->removeCss('btn-default')
+            ->addCss('btn btn-lg btn-primary btn-ss');
 
         if (!$this->institution) {
             $this->form->appendField(new Event\Link('forgotPassword', \Tk\Uri::create('/recover.html'), ''))
