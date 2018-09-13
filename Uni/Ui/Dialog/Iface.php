6cc606f3d294e009dff8a20563e3cb12804cda57
mifsudm <mifsudm@unimelb.edu.au>
2018-09-13 11:03:16 +1000

    ~Auto: Commit

--- Uni/Ui/Dialog/Iface.php
+++ Uni/Ui/Dialog/Iface.php
@@ -150,8 +150,8 @@ class Iface extends \Dom\Renderer\Renderer
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" id="exampleModalLabel" var="title">New message</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body" var="body">
         
