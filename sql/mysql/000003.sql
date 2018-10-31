b31835b43ce9aad136398014465d8c79625c7b5b
mifsudm <mifsudm@unimelb.edu.au>
2018-11-01 09:44:17 +1100

    ~Auto: Commit

--- sql/mysql/000003.sql
+++ sql/mysql/000003.sql
@@ -5,18 +5,19 @@
 -- ---------------------------------
 
 
-ALTER TABLE institution ADD address varchar(255) DEFAULT '' NULL;
-ALTER TABLE institution ADD postcode varchar(16) DEFAULT '' NULL AFTER address;
-ALTER TABLE institution ADD country varchar(128) DEFAULT '' NULL AFTER address;
-ALTER TABLE institution ADD state varchar(128) DEFAULT '' NULL AFTER address;
-ALTER TABLE institution ADD city varchar(128) DEFAULT '' NULL AFTER address;
-ALTER TABLE institution ADD street varchar(128) DEFAULT '' NULL AFTER address;
 ALTER TABLE institution ADD feature varchar(255) DEFAULT '' NULL AFTER logo;
 ALTER TABLE institution ADD phone varchar(32) DEFAULT '' NULL AFTER email;
 
-ALTER TABLE institution ADD `map_zoom` DECIMAL(4, 2) NOT NULL DEFAULT 14 AFTER postcode;
-ALTER TABLE institution ADD `map_lng` DECIMAL(11, 8) NOT NULL DEFAULT 0 AFTER postcode;
-ALTER TABLE institution ADD `map_lat` DECIMAL(11, 8) NOT NULL DEFAULT 0 AFTER postcode;
+ALTER TABLE institution ADD address varchar(255) DEFAULT '' NULL AFTER feature;
+ALTER TABLE institution ADD country varchar(128) DEFAULT '' NULL AFTER feature;
+ALTER TABLE institution ADD postcode varchar(16) DEFAULT '' NULL AFTER feature;
+ALTER TABLE institution ADD state varchar(128) DEFAULT '' NULL AFTER feature;
+ALTER TABLE institution ADD city varchar(128) DEFAULT '' NULL AFTER feature;
+ALTER TABLE institution ADD street varchar(128) DEFAULT '' NULL AFTER feature;
+
+ALTER TABLE institution ADD `map_zoom` DECIMAL(4, 2) NOT NULL DEFAULT 14 AFTER address;
+ALTER TABLE institution ADD `map_lng` DECIMAL(11, 8) NOT NULL DEFAULT 0 AFTER address;
+ALTER TABLE institution ADD `map_lat` DECIMAL(11, 8) NOT NULL DEFAULT 0 AFTER address;
 
 
 alter table user drop column display_name;
