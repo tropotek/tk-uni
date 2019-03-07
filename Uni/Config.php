856eeb24d28c0079b4e18f6a0a61b2d60e8a4f8c
Tropotek <info@tropotek.com>
2019-03-08 09:09:57 +1100

    ~Auto: Commit

--- Uni/Config.php
+++ Uni/Config.php
@@ -134,7 +134,8 @@ class Config extends \Bs\Config
                 $routePath = $this->getRouteCollection()->get($route)->getPath();
 //                vd($route);
 //                vd($this->getRouteCollection()->get($route)->getPath());
-                if (!$subject && $this->getSession()->has(self::SID_SUBJECT) && strpos($routePath, '/{subjectCode}') !== -1) {
+                vd($routePath, strpos($routePath, '/{subjectCode}'));
+                if (!$subject && $this->getSession()->has(self::SID_SUBJECT) && strpos($routePath, '/{subjectCode}') !== false) {
                     $subject = $this->getSubjectMapper()->find(self::getSession()->get(self::SID_SUBJECT));
                 }
                 $this->set('subject', $subject);
