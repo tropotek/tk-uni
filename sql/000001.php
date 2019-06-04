cdf4a2c53e5dfb1b3cc229e20ded7a8c0581ac27
mifsudm <mifsudm@unimelb.edu.au>
2019-06-04 10:42:45 +1000

    ~Auto: Commit

--- sql/000001.php
+++ sql/000001.php
@@ -7,12 +7,12 @@ try {
     $db = $config->getDb();
 
     $subjectInfo = $db->getTableInfo('subject');
-    vd($subjectInfo);
-    if (!in_array('publish', $subjectInfo)) {
+    var_dump($subjectInfo);
+    if (!array_key_exists('publish', $subjectInfo)) {
         $sql = sprintf('alter table subject add publish TINYINT(1) default 1 not null after date_end;');
         $db->exec($sql);
     }
-    if (!in_array('notify', $subjectInfo)) {
+    if (!array_key_exists('notify', $subjectInfo)) {
         $sql = sprintf('alter table subject add notify TINYINT(1) default 1 not null after date_end;');
         $db->exec($sql);
     }
