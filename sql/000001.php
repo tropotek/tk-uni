8215bf7c81cfbb5b023d099361853a8b549b9004
mifsudm <mifsudm@unimelb.edu.au>
2019-06-05 10:49:11 +1000

    ~Auto: Commit

--- sql/000001.php
+++ sql/000001.php
@@ -4,19 +4,25 @@ try {
     $config = \App\Config::getInstance();
     $db = $config->getDb();
 
-    $subjectInfo = $db->getTableInfo('subject');
-    if (!array_key_exists('publish', $subjectInfo)) {
+    $tableInfo = $db->getTableInfo('subject');
+    if (!array_key_exists('publish', $tableInfo)) {
         $sql = sprintf('alter table subject add publish TINYINT(1) default 1 not null after date_end;');
         $db->exec($sql);
     }
-    if (!array_key_exists('notify', $subjectInfo)) {
+    if (!array_key_exists('notify', $tableInfo)) {
         $sql = sprintf('alter table subject add notify TINYINT(1) default 1 not null after date_end;');
         $db->exec($sql);
     }
-//    if (!in_array('email', $subjectInfo)) {
-//        $sql = sprintf('alter table subject add email VARCHAR(255) default \'\' not null after code;');
-//        $db->exec($sql);
-//    }
+
+    $tableInfo = $db->getTableInfo('subject_pre_enrollment');
+    if (!array_key_exists('username', $tableInfo)) {
+        $sql = sprintf('ALTER TABLE subject_pre_enrollment ADD username VARCHAR(64) DEFAULT \'\' NOT NULL;');
+        $db->exec($sql);
+    }
+    if (!array_key_exists('email', $tableInfo)) {
+        $sql = sprintf('ALTER TABLE subject_pre_enrollment MODIFY COLUMN email varchar(168) NOT NULL DEFAULT \'\' AFTER uid;');
+        $db->exec($sql);
+    }
 
 } catch (\Tk\Db\Exception $e) {
 }
