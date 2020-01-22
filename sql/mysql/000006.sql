99577414cffcc0e996c2d1aed205d495a60009f1
mifsudm <mifsudm@unimelb.edu.au>
2020-01-22 12:42:42 +1100

    ~Auto: Commit

---
+++ sql/mysql/000006.sql
@@ -0,0 +1,30 @@
+-- ---------------------------------
+-- Update SQL
+--
+-- Author: Michael Mifsud <info@tropotek.com>
+-- ---------------------------------
+
+
+
+-- Create the new name fields
+alter table user
+    add name_first varchar(128) default '' not null after name;
+alter table user
+    add name_last varchar(128) default '' not null after name_first;
+
+-- Fill in the name fields
+UPDATE user SET name_first = SUBSTRING(name, 1, LOCATE(' ', name) - 1);
+UPDATE user SET name_last = SUBSTRING(name, LOCATE(' ', name) + 1);
+
+-- Move the display name field as I think we may keep this. (Uni only)
+-- alter table user modify display_name varchar(255) default '' not null after name_last;
+
+
+
+
+
+
+
+
+
+
