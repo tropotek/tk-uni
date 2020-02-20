4b5d45d2b23f22f0c0e07954fa8cc6cccfd5e9da
mifsudm <mifsudm@unimelb.edu.au>
2020-02-20 12:08:01 +1100

    ~Auto: Commit

---
+++ sql/mysql/000007.sql
@@ -0,0 +1,24 @@
+-- ---------------------------------
+-- Update SQL
+--
+-- Author: Michael Mifsud <info@tropotek.com>
+-- ---------------------------------
+
+
+RENAME TABLE user_permission TO _user_role_permission;
+RENAME TABLE user_role TO _user_role;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
