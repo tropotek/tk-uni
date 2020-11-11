d7c8a33377ac0c4339afa9b96301b41f168b5052
tropotek <info@tropotek.com>
2020-11-11 12:19:34 +1100

    ~Auto: Commit

--- Uni/Event/StatusEvent.php
+++ Uni/Event/StatusEvent.php
@@ -6,74 +6,9 @@ namespace Uni\Event;
  * @author Michael Mifsud <info@tropotek.com>
  * @see http://www.tropotek.com/
  * @license Copyright 2016 Michael Mifsud
+ * @deprecated use \Bs\Event\StatusEvent
  */
-class StatusEvent extends \Tk\Event\Event
+class StatusEvent extends \Bs\Event\StatusEvent
 {
 
-    /**
-     * @var \Uni\Db\Status
-     */
-    protected $status = null;
-
-    /**
-     * @var array
-     */
-    protected $messageList = array();
-
-
-    /**
-     * constructor.
-     *
-     * @param \Uni\Db\Status $status
-     */
-    public function __construct($status)
-    {
-        $this->status = $status;
-    }
-
-    /**
-     * @return \Uni\Db\Status
-     */
-    public function getStatus()
-    {
-        return $this->status;
-    }
-
-    /**
-     * If false events should not send emails to external users.
-     *
-     * @return bool
-     */
-    public function getNotify()
-    {
-        return $this->status->notify;
-    }
-
-    /**
-     * @param $message
-     * @return $this
-     */
-    public function addMessage($message)
-    {
-        $this->messageList[] = $message;
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getMessageList()
-    {
-        return $this->messageList;
-    }
-
-    /**
-     * @param array $list
-     * @return $this
-     */
-    public function setMessageList($list = array())
-    {
-        $this->messageList = $list;
-        return $this;
-    }
 }
